7c86ecb2d60ef46afbc3e40d752970b9044bb840
Siddharth Venkatesh <siddhuv93@gmail.com>
2022-11-23 23:00:01 +0530

    Added Kafka brokers Added table UI

--- dashboard/src/App.jsx
+++ dashboard/src/App.jsx
@@ -1,34 +1,105 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import {
+    createColumnHelper,
+    flexRender,
+    getCoreRowModel,
+    useReactTable,
+} from '@tanstack/react-table';
+
+import './App.css';
+
+const defaultData = [
+    {
+        firstName: 'tanner',
+        lastName: 'linsley',
+        age: 24,
+        visits: 100,
+        status: 'In Relationship',
+        progress: 50,
+    },
+    {
+        firstName: 'tandy',
+        lastName: 'miller',
+        age: 40,
+        visits: 40,
+        status: 'Single',
+        progress: 80,
+    },
+    {
+        firstName: 'joe',
+        lastName: 'dirte',
+        age: 45,
+        visits: 20,
+        status: 'Complicated',
+        progress: 10,
+    },
+];
+
+const columnHelper = createColumnHelper();
+
+const columns = [
+    columnHelper.accessor('firstName', {
+        cell: info => info.getValue(),
+    }),
+    columnHelper.accessor(row => row.lastName, {
+        id: 'lastName',
+        cell: info => <i>{info.getValue()}</i>,
+        header: () => <span>Last Name</span>,
+    }),
+    columnHelper.accessor('age', {
+        header: () => 'Age',
+        cell: info => info.renderValue(),
+    }),
+    columnHelper.accessor('visits', {
+        header: () => <span>Visits</span>,
+    }),
+    columnHelper.accessor('status', {
+        header: 'Status',
+    }),
+    columnHelper.accessor('progress', {
+        header: 'Profile Progress',
+    }),
+];
 
 function App() {
-  const [count, setCount] = useState(0)
+    const table = useReactTable({
+        data: defaultData,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+    })
 
-  return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
-  )
+    return (
+        <div className="App">
+            <table>
+                <thead>
+                    {table.getHeaderGroups().map(headerGroup => (
+                        <tr key={headerGroup.id}>
+                            {headerGroup.headers.map(header => (
+                                <th key={header.id}>
+                                    {header.isPlaceholder
+                                        ? null
+                                        : flexRender(
+                                            header.column.columnDef.header,
+                                            header.getContext()
+                                        )}
+                                </th>
+                            ))}
+                        </tr>
+                    ))}
+                </thead>
+                <tbody>
+                    {table.getRowModel().rows.map(row => (
+                        <tr key={row.id}>
+                            {row.getVisibleCells().map(cell => (
+                                <td key={cell.id}>
+                                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                                </td>
+                            ))}
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </div>
+    )
 }
 
 export default App
